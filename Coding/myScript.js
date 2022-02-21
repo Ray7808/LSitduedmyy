c5cf50e5317d539f4c11b99defe3f01d4ef916b7
Ray7808 <yuwang7808@gmail.com>
2022-02-21 22:56:12 +0800

    2/21

--- Coding/myScript.js
+++ Coding/myScript.js
@@ -1,1 +1,11 @@
-console.log(12 === "12")
+var age = 70
+
+if (age >= 65) {
+    console.log("Old man")
+}
+else if (age <= 65 && age >= 20) {
+    console.log("young man")
+}
+else {
+    console.log("Kid")
+}
