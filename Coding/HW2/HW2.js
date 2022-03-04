2fb039225be3d9f415f4c217806f9e112d9b12e7
Ray7808 <yuwang7808@gmail.com>
2022-03-04 13:18:32 +0800

    3/4

--- Coding/HW2/HW2.js
+++ Coding/HW2/HW2.js
@@ -1,20 +1,63 @@
 // homework 2.1
-function printStars(n) {
-    for (let i = 0; i < n; i++) {
-        console.log('*')
-    }
-}
+// function printStars(n) {
+//     for (let i = 0; i < n; i++) {
+//         console.log('*')
+//     }
+// }
 
-printStars(3)
+// printStars(3)
 
 //homework 2.2 忘記留檔惹
 
 //homework 2.3
-function reverse(n) {
-    for (let i = n.length - 1; i >= 0; i--) {
-        console.log(n[i])
-    }
+// function reverse(n) {
+//     for (let i = n.length - 1; i >= 0; i--) {
+//         console.log(n[i])
+//     }
 
-}
+// }
 
-reverse('.hello')
+// reverse('.hello')
+
+//homework 2.4
+// function printFactor(n) {
+//     for (let i = 1; i <= n; i++) {
+//         if (n % i === 0) {
+//             console.log(i)
+//         }
+//     }
+// }
+
+// printFactor(7)
+
+//homework 2.5
+// function NewJoin(a, b) {
+//     let Final = []
+//     for (let i = 0; i < a.length; i++) {
+//         if (i != a.length - 1) {
+//             Final += a[i] + b
+//         }
+//         else {
+//             Final += a[i]
+//         }
+
+//     }
+//     return Final
+// }
+// console.log(NewJoin([1, 2, 3], '!'))
+
+// function NewRepeat(a, b) {
+//     let Final = []
+//     for (let i = 0; i < b; i++) {
+//         Final += a
+//     }
+//     return Final
+// }
+// console.log(NewRepeat('a', 5))
+
+//homework2.6
+//一：他所有數值要大於0
+//二：第i個值要等於前兩項的加總
+//假如上述兩者有其中一個未符合即視為invalid
+
+//Challenge
