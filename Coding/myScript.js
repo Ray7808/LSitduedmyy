6f8117f10d2fdc4b6d31b27dda3a29a4305dae7f
Ray7808 <yuwang7808@gmail.com>
2022-03-07 20:38:08 +0800

    3/7

--- Coding/myScript.js
+++ Coding/myScript.js
@@ -1,90 +1,51 @@
-// common input and output example
+// LIOJ 1036：Array reverse
+function reverse(arr) {
 
-var readline = require('readline');
-const { domainToASCII } = require('url');
-var rl = readline.createInterface({
-    input: process.stdin
-});
+}
 
-var lines = [] // store all the input message
+// LIOJ 1037：Array filter
+// 這題 callback 會是一個 function，但難度會比較高
+// 若是做不出來，也可以調整參數
+function filter(arr, callback) {
 
-rl.on('line', function (line) {
+}
 
-    lines.push(line)
-})
+// LIOJ 1038：Array indexOf
+function indexOf(arr, searchElement) {
 
-rl.on('close', function () {
+}
 
-    solve(lines)
+// LIOJ 1039：Array fill
+function fill(arr, value) {
 
-})
+}
 
-function solve(lines) {
+// LIOJ 1040：Array join
+function join(arr, separator) {
 
-    let n = Number(lines[0])
-    let dots = []
-    for (let i = 1; i < lines.length; i++) {
-        let tmp = lines[i].split(' ')
-        dots.push({
-            x: Number(tmp[0]),
-            y: Number(tmp[1])
-        })
-    }
-    //console.log(dots)
+}
 
-    let min = Infinity
-    let ans = null
-    for (let i = 0; i < dots.length; i++) {
-        for (let j = i + 1; j < dots.length; j++) {
-            let dis = distance(
-                dots[i].x, dots[i].y, dots[j].x, dots[j].y
-            )
-            if (dis < min) {
-                min = dis
-                ans = {
-                    x1: dots[i].x,
-                    y1: dots[i].y,
-                    x2: dots[j].x,
-                    y2: dots[j].y
-                }
-            }
-        }
-    }
-    //console.log(ans)
+// LIOJ 1041：String trim
+function trim(str) {
 
-    if (ans.x1 > ans.x2) {
-        console.log(ans.x2 + ' ' + ans.y2)
-        console.log(ans.x1 + ' ' + ans.y1)
-    }
-    else if (ans.x1 < ans.x2) {
-        console.log(ans.x1 + ' ' + ans.y1)
-        console.log(ans.x2 + ' ' + ans.y2)
-    }
-    else {
-        if (ans.y1 > ans.y2) {
-            console.log(ans.x2 + ' ' + ans.y2)
-            console.log(ans.x1 + ' ' + ans.y1)
-        }
-        else {
-            console.log(ans.x1 + ' ' + ans.y1)
-            console.log(ans.x2 + ' ' + ans.y2)
-        }
-    }
+}
 
-    function distance(x1, y1, x2, y2) {
-        let dis = Math.sqrt(
-            abs(x1 - x2) * abs(x1 - x2) +
-            abs(y1 - y2) * abs(y1 - y2)
-        )
-        return dis
-    }
+// LIOJ 1042：String toLowerCase
+function toLowerCase(str) {
 
-    function abs(n) {
-        if (n < 0) {
-            return -n
-        }
-        else {
-            return n
-        }
-    }
+}
+
+// LIOJ 1043：String endsWith
+function endsWith(str, searchString) {
+
+}
+
+// LIOJ 1044：String padEnd
+function padEnd(str, targetLength, padString) {
+
+}
+
+// LIOJ 1045：String slice
+function slice(str, beginIndex, endIndex) {
+
 }
