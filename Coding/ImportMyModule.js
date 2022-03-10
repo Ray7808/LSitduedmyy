14822bbca7cab5489bc21aee433e3daa38f80baf
Ray7808 <yuwang7808@gmail.com>
2022-03-10 23:17:29 +0800

    3/10

---
+++ Coding/ImportMyModule.js
@@ -0,0 +1,18 @@
+//Import module
+let myModule = require('./myModule.js')
+
+//*Method 1 import function double
+// console.log(myModule) // output : Function: double
+// console.log(myModule(2)) // output : 4
+
+//*Method 2 import a array
+// console.log(myModule)
+
+//*Method 3 import an object
+//* You can use the value, function inside the object
+// console.log(myModule.double(2), myModule.triple(3))
+
+////////////////////////////////////
+//*Only import some part of myModule
+console.log(myModule)
+console.log(myModule.double(2))
--- Coding/myModule.js
+++ Coding/myModule.js
@@ -1,5 +1,29 @@
+//   Export all the comports
+//*For other people to use
+
 function double(n) {
     return n * 2
 }
 
-module.exports = [1, 2, 3]
+//*Method 1 export a funciton
+//*export module (function double)
+// module.exports = double
+
+//*Method 2 export a array
+// module.exports = [1, 2, 3]
+
+//*Method 3 export an object
+// let obj = {
+//     double: double,
+//     triple: function (n) {
+//         return n * 3
+//     }
+// }
+// module.exports = obj
+
+/////////////////////////////////
+//*Only export some part of myModule
+exports.double = double
+exports.triple = function (n) {
+    return n * 3
+}
