a586373290e95237dd9bf3b6c53b7886e8c2d05d
Ray7808 <yuwang7808@gmail.com>
2022-03-12 15:07:22 +0800

    3/12

--- Coding/sum.test.js
+++ Coding/sum.test.js
@@ -1,7 +1,17 @@
 const sum = require('./sum.js')
 
-test('add 1 + 2 to equal 3', () => {
-    expect(sum(1, 2)).toBe(3)
+
+//describe what you use for testing
+describe('Test sum function', function () {
+    test('add 1 + 2 to equal 3', () => {
+        expect(sum(1, 2)).toBe(3)
+    })
+    test('add 3 + 5 to equal 8', () => {
+        expect(sum(3, 5)).toBe(8)
+    })
+    test('add 10 + 20 to equal 30', () => {
+        expect(sum(10, 20)).toBe(30)
+    })
 })
 
 
